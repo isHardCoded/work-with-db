6a20624175fa2f10dcbf2aaecb155c54d8eeea45
isHardCoded <ishardcoded@gmail.com>
2026-04-03 18:01:49 +0300

    Добавьте файлы проекта.

---
+++ workWithDb/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+using System.Diagnostics;
+
+namespace workWithDb.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=store_db";
+
+        public MainWindowViewModel()
+        {
+            using var connection = new NpgsqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Debug.WriteLine("Connection opened successfully.");
+            }
+            catch (NpgsqlException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+
+            string sql = "SELECT id, name, price FROM products";
+
+            using var command = new NpgsqlCommand(sql, connection);
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string name = reader.GetString(1);
+                decimal price = reader.GetDecimal(2);
+
+                Debug.WriteLine($"{id}. {name} - {price}$");
+            }
+
+            connection.Close();
+
+        }
+    }
+}
---
+++ workWithDb/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace workWithDb.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
