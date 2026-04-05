0a0afabee78fbff6a4b0c0a6014163123db27fd1
isHardCoded <ishardcoded@gmail.com>
2026-04-05 12:53:45 +0300

    feat: вывели данные в xaml

--- workWithDb/ViewModels/MainWindowViewModel.cs
+++ workWithDb/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,17 @@
-﻿using Npgsql;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Npgsql;
+using System.Collections.Generic;
 using System.Diagnostics;
+using workWithDb.Models;
 
 namespace workWithDb.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=store_db";
+        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=p511_db";
+
+        [ObservableProperty]
+        private List<User> users = new();
 
         public MainWindowViewModel()
         {
@@ -20,8 +26,7 @@ namespace workWithDb.ViewModels
                 Debug.WriteLine(e.Message);
             }
 
-
-            string sql = "SELECT id, name, price FROM products";
+            string sql = "SELECT * FROM users";
 
             using var command = new NpgsqlCommand(sql, connection);
             using var reader = command.ExecuteReader();
@@ -30,9 +35,18 @@ namespace workWithDb.ViewModels
             {
                 int id = reader.GetInt32(0);
                 string name = reader.GetString(1);
-                decimal price = reader.GetDecimal(2);
+                string surname = reader.GetString(2);
+                int age = reader.GetInt32(3);
+                string email = reader.GetString(4);
 
-                Debug.WriteLine($"{id}. {name} - {price}$");
+                users.Add(new User()
+                {
+                    Id = id,
+                    Name = name,
+                    Surname = surname,
+                    Age = age,
+                    Email = email
+                });  
             }
 
             connection.Close();
